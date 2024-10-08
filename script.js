f0e29c131d1eed70c1d8e0efab6ee6aac1b03f57
cvtzv <kdevsvyaz@gmail.com>
2024-10-08 19:41:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-// Replace with your server's IP and port
-const serverIP = 'exp.komarucraft.pw';
-const serverPort = '25565';
+// script.js
+const altIpDropdown = document.querySelector('.alt-ip-dropdown');
+const altIpBtn = document.querySelector('.alt-ip-btn');
 
-// Function to get server status from MCSRVSTAT API
-async function fetchServerStatus() {
-    const response = await fetch(`https://api.mcsrvstat.us/2/${serverIP}:${serverPort}`);
-    const data = await response.json();
-
-    if (data.online) {
-        document.getElementById('server-status').innerText = 'Server is Online!';
-        document.getElementById('players-online').innerText = data.players.online;
-    } else {
-        document.getElementById('server-status').innerText = 'Server is Offline';
-    }
-}
-
-// Fetch server status when page loads
-window.onload = fetchServerStatus;
+altIpBtn.addEventListener('click', () => {
+    altIpDropdown.classList.toggle('active');
+});
