a18d4074ff74dd633744fbc404399522a5452119
cvtzv <kdevsvyaz@gmail.com>
2024-10-08 20:48:26 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const altIpDropdown = document.querySelector('.alt-ip-dropdown');
-const altIpBtn = document.querySelector('.alt-ip-btn');
-
-altIpBtn.addEventListener('click', () => {
-    altIpDropdown.classList.toggle('active');
-});
